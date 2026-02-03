1a59df0a8054149e166abab4dcc9e344cce7433a
Robbhedonic <143946750+Robbhedonic@users.noreply.github.com>
2026-02-03 15:34:13 +0100

    Beginner done

--- callback_excercises.js
+++ callback_excercises.js
@@ -1,117 +1,87 @@
 // Challenge 1
 /*  Create a function addTwo that accepts one input and adds 2 to it. */
-
-const addTwo = (num: number): number => {
-  return num + 2;
+var addTwo = function (num) {
+    return num + 2;
 };
-
 // const addTwo = (num : number): number => num + 2;
 // To check if you've completed it, uncomment these console.logs!
 console.log(addTwo(3));
 console.log(addTwo(10));
-
-
-
 // Challenge 2
-/* 
+/*
 Create a function addS that accepts one input and adds an "s" to it.
 */
-
-const addS = (word: string): string => {
- return word + "s";
-}
-
+var addS = function (word) {
+    return word + "s";
+};
 // const addS = (word: string): string => word + "s";
 // uncomment these to check your work
 console.log(addS("pizza"));
 console.log(addS("bagel"));
-
 // // ________________________________________________________________________________________________
 // // Challenge 3
-/* 
+/*
 Create a function called map that takes two inputs:
 1. An array of numbers (a list of numbers)
 2. A 'callback' function - a function that is applied to each element of the array (inside of the function 'map')
 Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
 */
-
-const map = (array: number[], callback: (num: number) => number): number[] => {
-  const result: number[] = [];
-  for (let i = 0; i < array.length; i++) {
-    result.push(callback(array[i]!));
-  }
-  return result;
+var map = function (array, callback) {
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        result.push(callback(array[i]));
+    }
+    return result;
 };
-
-
-
-console.log(map([1, 2, 3] as number[], addTwo));
-
-
-
+console.log(map([1, 2, 3], addTwo));
 // // ________________________________________________________________________________________________
 // Challenge 4
-/* 
-The function forEach takes an array and a callback, and runs the callback on each element of the array. 
+/*
+The function forEach takes an array and a callback, and runs the callback on each element of the array.
 forEach does not return anything.
 */
-
-function forEach(array: string[], callback:(char: string)=> void ): void  {
-    for(let i = 0; i < array.length; i++){
-        callback(array[i]!)
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i]);
     }
 }
-
-let alphabet = "";
-const letters = ["a", "b", "c", "d"];
+var alphabet = "";
+var letters = ["a", "b", "c", "d"];
 forEach(letters, function (char) {
-  alphabet += char;
+    alphabet += char;
 });
 console.log(alphabet);
-
 // should output abcd
-
-
 // // ________________________________________________________________________________________________
 // Challenge 5
-/* 
-Rebuild your map function, this time instead of using a for loop, use your own forEach function that you just defined. 
+/*
+Rebuild your map function, this time instead of using a for loop, use your own forEach function that you just defined.
 Call this new function mapWith.//console.log(mapWith([1, 2, 3], addTwo));
 */
-
-function ForEach<T>(array: T[], callback: (item: T) => void): void {
-  for (let i = 0; i < array.length; i++) {
-    callback(array[i]!);
-  }
+function ForEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i]);
+    }
 }
-
-
-function mapWith<T,U>(array: T[], callback: (item:T)=> U): U[]{
-    const result: U[] = [];
-    ForEach(array, item => result.push(callback(item)));
+function mapWith(array, callback) {
+    var result = [];
+    ForEach(array, function (item) { return result.push(callback(item)); });
     return result;
 }
-
-console.log(mapWith([1, 2, 3], addTwo)); 
-
+console.log(mapWith([1, 2, 3], addTwo));
 // should output [ 3, 4, 5 ]
-
-
 // // Challenge 6
 // /* 
 // The function reduce takes an array and reduces the elements to a single value. 
 // For example it can sum all the numbers, multiply them, 
 // or any operation that you can put into a function.
 // */
-
 // const nums = [4, 1, 3];
 // const add = function (a, b) {
 //   return a + b;
 // };
 // console.log(reduce(nums, add, 0))
-
 // //should output 8
-
 // // ________________________________________________________________________________________________
 // // Challenge 7
 // /* Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
@@ -120,16 +90,13 @@ console.log(mapWith([1, 2, 3], addTwo));
 //   intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
 // );
 // // should log: [5, 15]
-
 // // ________________________________________________________________________________________________
 // // Challenge 8
 // /* 
 // Construct a function union that compares input arrays and returns a new array that contains all elements. If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting from the first element of the first input array. BONUS: Use reduce!
 // */
-
 // console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // // should log: [5, 10, 15, 88, 1, 7, 100]
-
 // // ________________________________________________________________________________________________
 // // Challenge 9
 // /* 
@@ -138,7 +105,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. 
 // If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
 // */
-
 // console.log(
 //   objOfMatches(
 //     ["hi", "howdy", "bye", "later", "hello"],
@@ -148,9 +114,7 @@ console.log(mapWith([1, 2, 3], addTwo));
 //     }
 //   )
 // );
-
 // // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
-
 // // ________________________________________________________________________________________________
 // // Challenge 10
 // /* 
@@ -158,7 +122,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // multiMap will return an object whose keys match the elements in the array of values. 
 // The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key.
 // */
-
 // console.log(
 //   multiMap(
 //     ["catfood", "glue", "beer"],
@@ -175,9 +138,7 @@ console.log(mapWith([1, 2, 3], addTwo));
 //     ]
 //   )
 // );
-
 // // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
-
 // // ________________________________________________________________________________________________
 // // Challenge 11
 // /* 
@@ -185,36 +146,29 @@ console.log(mapWith([1, 2, 3], addTwo));
 // objectFilter will return a new object. 
 // The new object will contain only the properties from the input object such that the property's value is equal to the property's key passed into the callback.
 // */
-
 // const cities = {
 //   London: "LONDON",
 //   LA: "Los Angeles",
 //   Paris: "PARIS",
 // };
 // console.log(objectFilter(cities, (city) => city.toUpperCase()));
-
 // // Should log { London: 'LONDON', Paris: 'PARIS'}
-
 // // ________________________________________________________________________________________________
 // // Challenge 12
 // /* Create a function majority that accepts an array and a callback. 
 // The callback will return either true or false. majority will iterate through the array and perform the callback on each element until it can be determined if the majority of the return values from the callback are true. 
 // If the number of true returns is equal to the number of false returns, majority should return false.
 //  */
-
 // const isOdd = function (num) {
 //   return num % 2 === 1;
 // };
 // console.log(majority([1, 2, 3, 4, 5, 7, 9, 11], isOdd));
 // // should log: true
-
 // console.log(majority([2, 3, 4, 5], isOdd)); 
 // // should log: false
-
 // // ________________________________________________________________________________________________
 // // Challenge 13
 // /* Create a function prioritize that accepts an array and a callback. The callback will return either true or false. prioritize will iterate through the array and perform the callback on each element, and return a new array, where all the elements that yielded a return value of true come first in the array, and the rest of the elements come second. */
-
 // const startsWithS = function (str) {
 //   return str[0] === "s" || str[0] === "S";
 // };
@@ -224,9 +178,7 @@ console.log(mapWith([1, 2, 3], addTwo));
 //     startsWithS
 //   )
 // );
-
 // // should log: ["seinfeld", "sunny", "curb", "rickandmorty", "friends"];
-
 // // ________________________________________________________________________________________________
 // // Challenge 14
 // /* 
@@ -235,14 +187,12 @@ console.log(mapWith([1, 2, 3], addTwo));
 // Each return value from the callback will be saved as a key on the object. 
 // The value associated with each key will be the number of times that particular return value was returned.
 // */
-
 // console.log(
 //   countBy([1, 2, 3, 4, 5], function (num) {
 //     if (num % 2 === 0) return "even";
 //     else return "odd";
 //   })
 // ); // should log: { odd: 3, even: 2 }
-
 // // ________________________________________________________________________________________________
 // // Challenge 15
 // /* 
@@ -251,15 +201,12 @@ console.log(mapWith([1, 2, 3], addTwo));
 // Each return value from the callback will be saved as a key on the object. 
 // The value associated with each key will be an array consisting of all the elements that resulted in that return value when passed into the callback.
 // */
-
 // const decimals = [1.3, 2.1, 2.4];
 // const floored = function (num) {
 //   return Math.floor(num);
 // };
 // console.log(groupBy(decimals, floored));
-
 // // should log: { 1: [1.3], 2: [2.1, 2.4] }
-
 // // ________________________________________________________________________________________________
 // // Challenge 16
 // /* 
@@ -268,7 +215,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // goodKeys will iterate through the object and perform the callback on each value. 
 // goodKeys will then return an array consisting only the keys whose associated values yielded a true return value from the callback.
 // */
-
 // const sunny = {
 //   mac: "priest",
 //   dennis: "calculating",
@@ -280,9 +226,7 @@ console.log(mapWith([1, 2, 3], addTwo));
 //   return str.slice(0, 4).toLowerCase() === "bird";
 // };
 // console.log(goodKeys(sunny, startsWithBird));
-
 // // should log: ['charlie', 'dee']
-
 // // ________________________________________________________________________________________________
 // // Challenge 17
 // /* 
@@ -292,20 +236,15 @@ console.log(mapWith([1, 2, 3], addTwo));
 // yields the same output as the same operation with the order of the functions reversed (passing the value into the second function, 
 // and then passing the output into the first function).
 // */
-
 // const multBy3 = (n) => n * 3;
 // const divBy4 = (n) => n / 4;
 // const subtract5 = (n) => n - 5;
 // console.log(commutative(multBy3, divBy4, 11));
 // // should log: true
-
 // console.log(commutative(multBy3, subtract5, 10));
 // // should log: false
-
 // console.log(commutative(divBy4, subtract5, 48));
 // // should log: false
-
-
 // // Challenge 18
 // /* 
 // Create a function objFilter that accepts an object and a callback. 
@@ -313,7 +252,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // using each key as input for the callback. If the output from the callback is equal to the corresponding value, 
 // then that key-value pair is copied into the new object. objFilter will return this new object.
 // */
-
 // const startingObj = {};
 // startingObj[6] = 3;
 // startingObj[2] = 1;
@@ -321,7 +259,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // const half = (n) => n / 2;
 // console.log(objFilter(startingObj, half));
 // // should log: { 2: 1, 6: 3 }
-
 // // ________________________________________________________________________________________________
 // // Challenge 19
 // /* 
@@ -329,7 +266,6 @@ console.log(mapWith([1, 2, 3], addTwo));
 // All the functions in the array will return true or false. 
 // rating should return the percentage of functions from the array that return true when the value is used as input.
 // */
-
 // const isEven = (n) => n % 2 === 0;
 // const greaterThanFour = (n) => n > 4;
 // const isSquare = (n) => Math.sqrt(n) % 1 === 0;
@@ -337,10 +273,8 @@ console.log(mapWith([1, 2, 3], addTwo));
 // const checks = [isEven, greaterThanFour, isSquare, hasSix];
 // console.log(rating(checks, 64));
 // // should log: 100
-
 // console.log(rating(checks, 66)); 
 // // should log: 75
-
 // // ________________________________________________________________________________________________
 // // Challenge 20
 // /* 
@@ -351,13 +285,11 @@ console.log(mapWith([1, 2, 3], addTwo));
 // and so forth, until we have an output from the last function in the array. 
 // pipe should return the final output.
 // */
-
 // const capitalize = (str) => str.toUpperCase();
 // const addLowerCase = (str) => str + str.toLowerCase();
 // const repeat = (str) => str + str;
 // const capAddlowRepeat = [capitalize, addLowerCase, repeat];
 // console.log(pipe(capAddlowRepeat, "cat"));
-
 // // should log: 'CATcatCATcat'
 // // ________________________________________________________________________________________________
 // // Challenge 21
@@ -367,20 +299,16 @@ console.log(mapWith([1, 2, 3], addTwo));
 // whose associated value (which will be a function) returns the largest number, 
 // when the subject is given as input.
 // */
-
 // const groupOfFuncs = {};
 // groupOfFuncs.double = (n) => n * 2;
 // groupOfFuncs.addTen = (n) => n + 10;
 // groupOfFuncs.inverse = (n) => n * -1;
 // console.log(highestFunc(groupOfFuncs, 5));
 // // should log: 'addTen'
-
 // console.log(highestFunc(groupOfFuncs, 11));
 // // should log: 'double'
-
 // console.log(highestFunc(groupOfFuncs, -20));
 // // should log: 'inverse'
-
 // // ________________________________________________________________________________________________
 // // Challenge 22
 // /* 
@@ -390,32 +318,25 @@ console.log(mapWith([1, 2, 3], addTwo));
 // and so on until every function in the array has been called. combineOperations should return the final value 
 // returned by the last function in the array.
 // */
-
 // function add100(num) {
 //   return num + 100;
 // }
-
 // function divByFive(num) {
 //   return num / 5;
 // }
-
 // function multiplyByThree(num) {
 //   return num * 3;
 // }
-
 // function multiplyFive(num) {
 //   return num * 5;
 // }
-
 // function addTen(num) {
 //   return num + 10;
 // }
-
 // console.log(combineOperations(0, [add100, divByFive, multiplyByThree]));
 // // Should output 60 -->
 // console.log(combineOperations(0, [divByFive, multiplyFive, addTen]));
 // // Should output 10
-
 // // ________________________________________________________________________________________________
 // // Challenge 23
 // /* 
@@ -424,19 +345,15 @@ console.log(mapWith([1, 2, 3], addTwo));
 // If the callback returns true, myFunc should return the index of the current element. 
 // If the callback never returns true, myFunc should return -1;
 // */
-
 // const numbers = [2, 3, 6, 64, 10, 8, 12];
 // const evens = [2, 4, 6, 8, 10, 12, 64];
-
 // function isOddAgain(num) {
 //   return num % 2 !== 0;
 // }
-
 // console.log(myFunc(numbers, isOddAgain));
 // // Output should be 1
 // console.log(myFunc(evens, isOddAgain));
 // // Output should be -1
-
 // // ________________________________________________________________________________________________
 // // Challenge 24
 // /* Write a function myForEach that accepts an array and a callback function. 
@@ -444,13 +361,10 @@ console.log(mapWith([1, 2, 3], addTwo));
 // The behavior of this function should mirror the functionality of the native .forEach() 
 // JavaScript array method as closely as possible.
 //  */
-
 // let sum = 0;
-
 // function addToSum(num) {
 //   sum += num;
 // }
-
 // const nums2 = [1, 2, 3];
 // myForEach(nums2, addToSum);
 // console.log(sum);
