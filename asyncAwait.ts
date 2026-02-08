888967f4711e7c36b05ea91e61814db0041473f2
Robbhedonic <143946750+Robbhedonic@users.noreply.github.com>
2026-02-09 00:05:05 +0100

    Done Part 1

--- asyncAwait.ts
+++ asyncAwait.ts
@@ -1,20 +1,33 @@
 
+// tsc asyncAwait.ts
+// node asyncAwait.js
 
-const flipCoin = () =>{
-    return new Promise((resolve, reject)=>{
-        let result = Math.random();
-        if(result > 0.5){
-            resolve("You win!")
-        } else {
-            reject("You lose!")
-        }
-    });
-};
+// const flipCoin = () =>{
+//     return new Promise((resolve, reject)=>{
+//         let result = Math.random();
+//         if(result > 0.5){
+//             resolve("You win!")
+//         } else {
+//             reject("You lose!")
+//         }
+//     });
+// };
 
-flipCoin()
-    .then((message)=> {
-        console.log(message);
+const flipCoin = () => {
+    return new Promise((resolve, reject) => {
+        const outcome = Math.random() > 0.5;
+        outcome ? resolve("You win!") : reject("You lose!");
     })
-    .catch((error)=> {
-        console.log(error)
-    });
+}
+
+const coinFlipResult = async () => {
+    try{
+        const result = await flipCoin();
+        console.log(result);
+    } 
+    catch(error) {
+        console.log(error);
+    }
+}
+
+coinFlipResult();
